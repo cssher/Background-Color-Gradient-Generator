1c86113a80fa7ef1c49166521e4abbb6e3c2a32f
Sher Ali <sherali2910.sa@gmail.com>
2019-09-08 18:23:34 +0530

    3 tone gradient code added

--- bg-script.js
+++ bg-script.js
@@ -1,6 +1,7 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
 var copyText = document.getElementById("myInput");
 
@@ -10,12 +11,14 @@ function setGradient() {
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
+	+", "
+	+color3.value
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	css.textContent = body.style.background+ ";";
 	
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
